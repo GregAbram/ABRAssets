d1ab6b5d91ce247ade199a7cd5b7d17399a58792
Greg Abram <gda@tacc.utexas.edu>
2025-04-01 12:54:06 -0500

    rearrangement 2

---
+++ Scripts/LookMove.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Diagnostics.Tracing;
+using System.Security.Cryptography;
+
+public class LookMove : CameraModel
+{
+    public override bool CameraController()
+    {	
+		bool save = false;
+
+		if(Input.GetKeyDown("r"))
+		{
+			transform.position = Vector3.zero;
+			transform.rotation = Quaternion.identity;
+			save = true;
+		}
+
+	    if (Input.GetMouseButtonDown(button))
+		{
+	        mouseIsDown = true;
+			lastPosition = Input.mousePosition;
+		}
+		else if (Input.GetMouseButtonUp(button))   
+		{
+			mouseIsDown = false;
+			save = true;
+		}
+	   
+	    if (mouseIsDown)
+	    {
+            Vector3 currentPosition = Input.mousePosition;
+            Vector3 deltaPosition = currentPosition - lastPosition;
+			lastPosition = currentPosition;
+
+            float inputX = deltaPosition.x * mouseRotationSensitivity;
+            float inputY = deltaPosition.y * mouseRotationSensitivity;
+			if (inputX != 0 || inputY != 0)
+			{
+				Quaternion q_r, q_u;
+            	q_r = Quaternion.AngleAxis(inputY, Camera.main.transform.right);
+            	q_u = Quaternion.AngleAxis(-inputX, Camera.main.transform.up);
+            	transform.rotation = q_r * q_u * transform.rotation;
+			}
+		}
+
+		float inputSW = Input.GetAxis("Mouse ScrollWheel") * mouseMovementSensitivity;
+		if (inputSW != 0)
+		{
+			if (Input.GetKey("space"))
+				transform.position += inputSW * transform.up;
+			else
+				transform.position += inputSW * transform.forward;
+		
+			save = true;
+
+		}	
+
+		return save;
+     }
+}
+
---
+++ Scripts/ScreenShot.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    static int shotCount = 0;
+    public string screenshot_template = "screenshot_{0}.png";
+    static string screenshot_cache = "";
+    void Start()
+    {
+        Configurator cfg = ScriptableObject.CreateInstance<Configurator>();
+        if (cfg.GetString("-screenshotCache", out screenshot_cache))
+        {
+            if (! Directory.Exists(screenshot_cache))
+            {
+                Debug.Log("Screenshot directory " + screenshot_cache + " does not exist");
+                screenshot_cache = "";
+            }
+            else
+            {
+                 Debug.Log("Using ScreenShot Cache: " + screenshot_cache);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) && screenshot_cache != "")
+        {
+            int multiplier;
+
+            string s;
+            Configurator cfg = ScriptableObject.CreateInstance<Configurator>();
+
+            if (cfg.GetString("-screenshotMultiplier", out s))
+                int.TryParse(s, out multiplier);
+            else
+                multiplier = 2;
+
+            string filename = string.Format(screenshot_template, DateTime.Now.ToString("MM-dd-yyyy-h-mm-tt"));
+
+            if (screenshot_cache != "")
+                filename = screenshot_cache + "/" + filename;
+
+            Debug.Log("Saving " + filename);
+            ScreenCapture.CaptureScreenshot(filename, multiplier);
+            shotCount = shotCount + 1;
+        }
+    }
+}
