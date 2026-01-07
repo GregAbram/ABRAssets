38deec95a0c2e19b0ee7623907b4d4c293cae1d3
gda <gda@tacc.utexas.edu>
2026-01-07 17:16:04 -0600

    PickHandler script got cleared somehow

---
+++ Runtime/PickHandler.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using IVLab.ABREngine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+//public class PickHandler : IVLab.ABREngine.ABRPicker.ABRPickHandler
+public class PickHandler : IVLab.ABREngine.ABRPicker.ABRPickHandler
+{
+    public override void onPick(ABRPicker.ABRPick pick)
+    {
+        IDataImpression idi = ABREngine.Instance.GetDataImpression(pick.guid);
+        if (idi == null)
+        {
+            Debug.Log("No data impression found for pick");
+            return;
+        }
+
+        string msg = "";
+
+        if (! pick.abrGameObject.TryGetComponent<EncodedGameObject>(out EncodedGameObject ego))
+        {
+            Debug.Log("No encoded game object on picked object");
+            return;
+        }
+
+        SimpleGlyphDataImpression gdi = idi as SimpleGlyphDataImpression;
+        if (gdi != null)
+        {
+            msg = msg + $"Type: Glyph\n";
+            msg = msg + $"Which: {pick.id}\n";
+            gdi.toggleHilite(ego, pick.id);
+        }
+
+        SimpleSurfaceDataImpression sdi = idi as SimpleSurfaceDataImpression;
+        if (sdi != null)
+        {            
+            msg = msg + $"Type: Surface\nWhich: {pick.id}\nbarycentric weights: {pick.barycentric_weights}\n";
+
+            sdi.toggleHilite(); 
+            SurfaceDatasetAccessor sda = new SurfaceDatasetAccessor(pick.dataset, idi);
+
+            if (sda.GetScalarValue(pick, out float v))
+                msg = msg + $"Scalar value: {v}\n";
+        }
+
+        SimpleLineDataImpression ldi = idi as SimpleLineDataImpression;
+        if (ldi != null)
+        {
+            msg = msg + $"Type: Line\nWhich: {pick.id}\n";
+            ldi.toggleHilite(pick.id);
+        }
+
+        ABREngine.Instance.Render();
+
+        msg = msg + $"Color variable: {idi.GetColorVariableName()}\n";
+        msg = msg + $"Point: {pick.point}\n";
+
+        Debug.Log("Pick:\n" + msg);
+    }
+}
